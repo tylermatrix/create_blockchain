472856b6fa3ac217cb5fca8cef5f089a48713c42
tylermatrix <tylermaatrix@gmail.com>
2021-09-30 20:38:18 -0400

    created env and started writing Blockchain class
    wrote init function and create_block in the blockchain class

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+#Module 1 - Create a blockchain 
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#create a blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, prev_hash= '0')
+        
+    def create_block(self, proof, prev_hash):
+        block = {'index' : len(self.chain) + 1,
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'prev_hash' : prev_hash
+                 }
+
+#mine a blockchain
