3fb6a8315a9ccb3a192a825c987483580666e962
tylermatrix <tylermaatrix@gmail.com>
2021-10-10 15:48:31 -0400

    update blockchain.py
    Created a function to mine blocks

--- blockchain.py
+++ blockchain.py
@@ -62,12 +62,31 @@ class Blockchain:
 app = Flask(__name__)
 
 #Create a blockchain
+blockchain = Blockchain()
+
+#mining a blockchain
+@app.route('./mine_block', methods=['GET', 'POST'])
+def mine_block():
+    prev_block = blockchain.get_prev_block()
+    prev_proof = prev_block['proof']
+    proof = blockchain.proof_of_work(prev_proof)
+    
+    prev_hash = blockchain.hash(prev_block)
+    
+    block = blockchain.create_block(proof, prev_hash)
+
+    response = {'message' : 'Congrats! You just mined a block!',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'prev_hash' : block['prev_hash']
+                }
+    return jsonify(response), 200   
 
 
 
 
-
-
+@app.route('./get_chain', methods=['GET'])
 
 
 
